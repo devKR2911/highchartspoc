dbd6eb1b384241dcacee6d622f75ae3fe82db9a8
Nitheesh <nitheeshkr115@gmail.com>
2019-10-08 00:54:02 +0530

    pagination

--- src/app/stacked-bar-graph/stacked-bar-graph.component.ts
+++ src/app/stacked-bar-graph/stacked-bar-graph.component.ts
@@ -22,6 +22,9 @@ noData(Highcharts);
 export class StackedBarGraphComponent implements OnInit {
   constructor() {}
   public itemsPerScreen = { id: 0, value: 5 };
+  public buttonArray = [];
+  public visibleData = [];
+  public chart;
 
   public graphOptions = [
     { id: 0, value: 5 },
@@ -109,13 +112,28 @@ export class StackedBarGraphComponent implements OnInit {
     series: []
   };
 
-  public onChange = function() {
-    console.log(this.itemsPerScreen);
-    // this.parseData();
+  public onPagination = function(index) {
+    this.visibleData = graphdata.slice(
+      this.itemsPerScreen.value * index,
+      (index + 1) * this.itemsPerScreen.value
+    );
+    this.parseData(true);
+  };
+
+  public onChange = function(bool) {
+    const totalCount = graphdata.length;
+    const buttonCount = Math.ceil(totalCount / this.itemsPerScreen.value);
+    this.buttonArray = [];
+    for (let i = 0; i < buttonCount; i++) {
+      this.buttonArray.push(i);
+    }
+    this.onPagination(0);
+    this.parseData(bool);
+
     // this.rebindGraph();
   };
 
-  public parseData = function() {
+  public parseData = function(bool) {
     const categories = [];
     const pensionData = { name: 'Pension', data: [], color: '#ff5622' };
     const salaryData = { name: 'Salary', data: [], color: '#9013fe' };
@@ -124,7 +142,7 @@ export class StackedBarGraphComponent implements OnInit {
       data: [],
       color: '#feab00'
     };
-    graphdata.forEach(item => {
+    this.visibleData.forEach(item => {
       categories.push(item.date);
       pensionData.data.push(item.value.Pension);
       salaryData.data.push(item.value.Salary);
@@ -133,12 +151,16 @@ export class StackedBarGraphComponent implements OnInit {
     const series = [pensionData, salaryData, extraPensionData];
     this.options.series = series;
     this.options.xAxis.categories = categories;
+    // this.chart.redraw();
+    if (bool && this.chart) {
+      this.chart.update(this.options);
+    }
   };
 
   public rebindGraph = function() {};
 
   ngOnInit() {
-    this.parseData();
-    Highcharts.chart('container', this.options);
+    this.onChange(false);
+    this.chart = Highcharts.chart('container', this.options);
   }
 }
